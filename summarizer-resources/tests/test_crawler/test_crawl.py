394d679e06f7f4acf5b41d8557e404ae44271959
Kerem Baskaya <kerembaskaya@gmail.com>
2023-03-25 21:59:34 -0700

    Complete primitive version of crawl with using googlemap api

--- summarizer-resources/tests/test_crawler/test_crawl.py
+++ summarizer-resources/tests/test_crawler/test_crawl.py
@@ -1,2 +1,40 @@
-def test_crawl():
-    assert True
+from summarizer.crawler import crawl
+
+import json
+import os
+from summarizer.crawler.crawl import CLIENT, merge_all_reviews
+from summarizer.crawler.crawl import google_api_reviews_crawler
+from summarizer.crawler.crawl import convert_google_reviews_format_to_records
+
+from summarizer.crawler.crawl import GooglePlaceRevivewRecord
+
+
+# def test_crawl():
+# x = google_api_reviews_crawler('Saint Frank Coffee', CLIENT)
+# I need to put sleep time for the test or mock.
+# TODO: But either way I should put some sleeping time to actual function.
+
+
+def test_convert_google_reviews_format_to_records():
+    review_records = convert_google_reviews_format_to_records(
+        "summarizer-resources/tests/test_data/reviews.json"
+    )
+    assert len(review_records) == 5
+    assert all(
+        [isinstance(obj, GooglePlaceRevivewRecord) for obj in review_records]
+    )
+
+
+def test_merge_all_reviews():
+    """
+    The logic of this test, the length of the merged reviews has to always equal or
+    bigger than the sum of the length of each review.
+    """
+    review_records = convert_google_reviews_format_to_records(
+        "summarizer-resources/tests/test_data/reviews.json"
+    )
+    all_reviews = merge_all_reviews(review_records)
+    length_of_the_all_reviews = sum(
+        [len(record.review_text) for record in review_records]
+    )
+    assert len(all_reviews) >= length_of_the_all_reviews
